d9dd645d5780aa55cc9d9b31ecb53943a3a061c7
Celie Baker <celie.n133@gmail.com>
2018-03-26 15:43:51 -0600

    this keyword doesn't seem to be working... when adding and removing items, actions are being taken accross every list

--- project2/action.js
+++ project2/action.js
@@ -2,20 +2,54 @@
 
 function createAList() {
     $('.list')
-        .append('<div class="toDoList editbox" contenteditable="true"> ' +
-                '<span class="listLabel">Click to edit list title</span> ' +
-                '</div>');
+        .append('<div class="toDoList">\n' +
+            '    <div class="top">\n' +
+            '        <div class="editbox" contenteditable="true">\n' +
+            '            <span class="listLabel">Click to edit list title</span>\n' +
+            '        </div>\n' +
+            '\n' +
+            '        <div class="iconsTop">\n' +
+            '            <div onclick="addToDoItem(this)">\n' +
+            '                <i class="fas fa-plus"></i>\n' +
+            '            </div>\n' +
+            '\n' +
+            '            <div class="deleteList" onclick="deleteAList(this)">\n' +
+            '                <i class="fas fa-trash-alt"></i>\n' +
+            '            </div>\n' +
+            '        </div>\n' +
+            '    </div>\n' +
+            '</div>');
 }
 
 function addToDoItem() {
     $('.toDoList')
-        .append('<div class="listItemContainer editbox" contenteditable="true">\n' +
-                '<span class="listItem">Click to edit list item</span>\n' +
-                '</div>');
+        .append('<div class="bottom">\n' +
+            '        <div class="listItemContainer">\n' +
+            '            <div class="bottomLeft">\n' +
+            '                <div class="square">\n' +
+            '                    <i class="far fa-square"></i>\n' +
+            '                </div>\n' +
+            '\n' +
+            '                <div class="editbox" contenteditable="true">\n' +
+            '                    <span class="listItem">Click to edit list item</span>\n' +
+            '                </div>\n' +
+            '            </div>\n' +
+            '\n' +
+            '            <div class="iconsBottom">\n' +
+            '                <div onclick="deleteAItem(this)">\n' +
+            '                    <i class="fas fa-minus"></i>\n' +
+            '                </div>\n' +
+            '            </div>\n' +
+            '        </div>\n' +
+            '    </div>');
 }
 
 function deleteAList() {
-    $('.toDoList').remove($(this));
+    $('.toDoList').remove();
+}
+
+function deleteAItem() {
+    $('.listItemContainer').remove();
 }
 
 
