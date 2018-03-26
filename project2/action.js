5ba75fa709222247e60179a9400e13e12f23e9ad
Celie Baker <celie.n133@gmail.com>
2018-03-26 15:06:36 -0600

    HTML and CSS are all done - just need to add JS functionality plus bonus extras

--- project2/action.js
+++ project2/action.js
@@ -1,11 +1,28 @@
-//<div class="editbox" contenteditable=true></div>
 //use .localstorage to store to-do list locally
 
 function createAList() {
-
-    let listName = $('.createInput').val();
-
-    $('.createdLists').append('<div>'+ listName + '</div>');
-
-    $('.createInput').val('');
+    $('.list')
+        .append('<div class="toDoList editbox" contenteditable="true"> ' +
+                '<span class="listLabel">Click to edit list title</span> ' +
+                '</div>');
 }
+
+function addToDoItem() {
+    $('.toDoList')
+        .append('<div class="listItemContainer editbox" contenteditable="true">\n' +
+                '<span class="listItem">Click to edit list item</span>\n' +
+                '</div>');
+}
+
+function deleteAList() {
+    $('.toDoList').remove($(this));
+}
+
+
+// $(document).ready(function() {
+//
+//     $('.fa-square').on('click', function() {
+//         $(this).toggleClass('fa-square fa-check-square');
+//     });
+//
+// });
