e35290cb3ae22abb23939e88d1d94d5ac479fc08
Celie Baker <celie.n133@gmail.com>
2018-03-26 18:22:35 -0600

    so i felt like the whole onclick thing in html is archaic and there are better ways to do things so i am using on.click jquery instead

--- project2/action.js
+++ project2/action.js
@@ -1,62 +1,63 @@
 //use .localstorage to store to-do list locally
 
-function createAList() {
-    $('.list')
-        .append('<div class="toDoList">\n' +
-            '    <div class="top">\n' +
-            '        <div class="editbox" contenteditable="true">\n' +
-            '            <span class="listLabel">Click to edit list title</span>\n' +
-            '        </div>\n' +
-            '\n' +
-            '        <div class="iconsTop">\n' +
-            '            <div onclick="addToDoItem(this)">\n' +
-            '                <i class="fas fa-plus"></i>\n' +
-            '            </div>\n' +
-            '\n' +
-            '            <div class="deleteList" onclick="deleteAList(this)">\n' +
-            '                <i class="fas fa-trash-alt"></i>\n' +
-            '            </div>\n' +
-            '        </div>\n' +
-            '    </div>\n' +
-            '</div>');
-}
+$(document).ready(function() {
 
-function addToDoItem() {
-    $('.toDoList')
-        .append('<div class="bottom">\n' +
-            '        <div class="listItemContainer">\n' +
-            '            <div class="bottomLeft">\n' +
-            '                <div class="square">\n' +
-            '                    <i class="far fa-square"></i>\n' +
-            '                </div>\n' +
-            '\n' +
-            '                <div class="editbox" contenteditable="true">\n' +
-            '                    <span class="listItem">Click to edit list item</span>\n' +
-            '                </div>\n' +
-            '            </div>\n' +
-            '\n' +
-            '            <div class="iconsBottom">\n' +
-            '                <div onclick="deleteAItem(this)">\n' +
-            '                    <i class="fas fa-minus"></i>\n' +
-            '                </div>\n' +
-            '            </div>\n' +
-            '        </div>\n' +
-            '    </div>');
-}
-
-function deleteAList() {
-    $('.toDoList').remove();
-}
-
-function deleteAItem() {
-    $('.listItemContainer').remove();
-}
+    $('.fa-square').on('click', function() {
+        $(this).toggleClass('fa-square fa-check-square');
+        $(this).closest('.listItemContainer').toggleClass('itemComplete')
+    });
 
 
-// $(document).ready(function() {
-//
-//     $('.fa-square').on('click', function() {
-//         $(this).toggleClass('fa-square fa-check-square');
-//     });
-//
-// });
+    $('.fa-trash-alt').on('click', function() {
+        $(this).closest('.toDoList').remove();
+    });
+
+
+    $('.fa-minus').on('click', function() {
+        $(this).closest('.bottom').remove();
+    });
+
+    $('.blueBtn').on('click', function() {
+        $('.list')
+            .append('<div class="toDoList">\n' +
+                '    <div class="top">\n' +
+                '        <div class="editbox" contenteditable="true">\n' +
+                '            <span class="listLabel">Click to edit list title</span>\n' +
+                '        </div>\n' +
+                '\n' +
+                '        <div class="iconsTop">\n' +
+                '            <div>\n' +
+                '                <i class="fas fa-plus"></i>\n' +
+                '            </div>\n' +
+                '\n' +
+                '            <div class="deleteList">\n' +
+                '                <i class="fas fa-trash-alt"></i>\n' +
+                '            </div>\n' +
+                '        </div>\n' +
+                '    </div>\n' +
+                '</div>');
+    });
+
+    $('.fa-plus').on('click', function(){
+        $(this).closest('.toDoList')
+            .append('<div class="bottom">\n' +
+                '        <div class="listItemContainer">\n' +
+                '            <div class="bottomLeft">\n' +
+                '                <div class="square">\n' +
+                '                    <i class="far fa-square"></i>\n' +
+                '                </div>\n' +
+                '\n' +
+                '                <div class="editbox" contenteditable="true">\n' +
+                '                    <span class="listItem">Click to edit list item</span>\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '\n' +
+                '            <div class="iconsBottom">\n' +
+                '                <div>\n' +
+                '                    <i class="fas fa-minus"></i>\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '        </div>\n' +
+                '    </div>');
+    });
+});
