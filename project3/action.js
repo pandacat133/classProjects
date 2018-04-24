03530797068094c7a9f313acda912f9d4013d932
Celie Baker <celie.n133@gmail.com>
2018-04-24 10:03:44 -0600

    added add score functionality and added a bunch of CSS

--- project3/action.js
+++ project3/action.js
@@ -3,7 +3,9 @@
 //let them select "champion", "pro", "men", "women" and then populate off of that
 
 let numPlayers = 4;
-let course;
+let allCourses;
+let selectCourse;
+let numberOfHoles = 18;
 
 loadDoc();
 
@@ -11,42 +13,100 @@ function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            course = JSON.parse(this.responseText);
-            console.log(course);
+            allCourses = JSON.parse(this.responseText);
+            console.log(allCourses);
 
 
-            let selTees = course.holes[0].tees;
-            for(let i = 0; i < selTees.length; i++) {
-                $('#teeSelect').append('<option value="'+ i +'">'+ selTees[i].teeName +'</option>');
+            let selCourses = allCourses.courses;
+            for(let i = 0; i < selCourses.length; i++) {
+                $('.coursesDropdown').append('<option value="'+ selCourses[i].id +'">'+ selCourses[i].name +'</option>');
             }
 
             //createCard();
         }
     };
-    xhttp.open("GET", "holes.txt", true);
+    xhttp.open("GET", "https://uxcobra.com/golfapi/courses.txt", true);
     xhttp.send();
 }
 
-function createCard() {
-    for(let i = 0; i < course.holes.length; i++) {
-        $('.right').append('<div id="col'+ i +'" class="column">\n' +
-        '                       <div class="cHeader">\n' +
-            '                       \n' + course.holes[i].name +
-            '                   </div>\n' +
-        '                   </div>');
+function getCourse(courseId) {
+    $('.teeDropdown').html('');
+    console.log(courseId);
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if(this.readyState === 4 && this.status === 200) {
+            selectCourse = JSON.parse(this.responseText);
+            console.log(selectCourse);
+
+            let holeOneTees = selectCourse.data.holes[0].teeBoxes;
+            for(let i = 0; i < holeOneTees.length; i++) {
+                $('.teeDropdown').append('<option value="'+ i +'">'+ holeOneTees[i].teeType +'</option>');
+            }
+
+        }
+    };
+
+    xhttp.open('GET', 'https://uxcobra.com/golfapi/course' + courseId + '.txt', true);
+    xhttp.send();
+}
+
+function setTee(teeIndex) {
+    $('.right').html('');
+
+    let myCourse = selectCourse.data.holes;
+    for(let i = 0; i < myCourse.length; i++){
+
+        let myTee = myCourse[i].teeBoxes[teeIndex];
+
+        $('.right').append('<div id="c'+ i +'" class="column">\n' +
+            '                <div class="cHeader">\n' +
+            '                    <div class="holes">\n' + (i+1) +
+            '                    </div>\n' +
+            '                    <div class="yards">\n' + myTee.yards +
+            '                    </div>\n' +
+            '                    <div class="hcp">\n' + myTee.hcp +
+            '                    </div>\n' +
+            '                    <div class="par">\n' + myTee.par +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '            </div>');
     }
 
     fillCard();
 }
 
 function fillCard() {
-    for(let p = 1; p <= numPlayers; p++) {
-        $('.left').append('<div class="playerLabel">\n' +
-            '                Player\n' + p +
-            '            </div>');
+    // for(let p = 1; p <= numPlayers; p++) {
+    //     $('.left').append('<div class="playerLabel">\n' +
+    //         '                Player\n' + p +
+    //         '            </div>');
+    //
+    //     for(let h = 0; h < course.holes.length; h++) {
+    //         $('#col'+ h).append('<input class="holeInput" id="p'+ p +' h'+ h +'" type="text">');
+    //     }
+    // }
 
-        for(let h = 0; h < course.holes.length; h++) {
-            $('#col'+ h).append('<input class="holeInput" id="p'+ p +' h'+ h +'" type="text">');
+    for(let p = 1; p <= numPlayers; p++) {
+        $('.left').append('<div class="playerLabel"><span class="fa fa-trash"></span>Player'+ p +'</div>');
+
+        let myHoles = selectCourse.data.holes;
+        for(let h = 0; h < myHoles.length; h++) {
+            $('#c' + h).append('<input onkeyup="addScore(p)" class="holeInput" id="p'+ p +' h'+ h +'" type="text">');
         }
     }
+}
+
+function addScore(myValue) {
+    let tempScore = 0;
+    for(let i = 1; i <= numberOfHoles; i++) {
+        let inValue = Number($('#p' + myValue + 'h' + i).val());
+        tempScore += inValue;
+    }
+
+    $('.totalP' + myValue).html(tempScore);
+}
+
+function deletePlayer(incPlayer) {
+    
 }
